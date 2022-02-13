ae2c6bdd896beff107d166c344b68f6830f18e8f
ulugbek7777 <ulugbek20037@gmail.com>
2022-02-13 16:31:39 +0500

    sidebar topbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
+import { useState } from "react";
 import {
   Routes,
   Route
 } from "react-router-dom";
 import Login from './components/login/Login';
+import SideBar from "./components/sidebar/SideBar";
+import Topbar from "./components/sidebar/TopBar";
 import { PrivateRoute } from './utils/PrivateRoute';
-
+import './App.css'
 function App() {
-  
+  const [sidebarIsOpen, setSidebarOpen] = useState(true);
+  const toggleSidebar = () => setSidebarOpen(!sidebarIsOpen);
   return (
     <div className="App">
+      <SideBar toggle={toggleSidebar} isOpen={sidebarIsOpen} />
+      <Topbar toggleSidebar={toggleSidebar} />
       <Routes>
         <Route path="/" element={<PrivateRoute><h1>/</h1></PrivateRoute>}/>
         <Route path="/login" element={<Login />}/>
---
+++ src/components/sidebar/SubMenu.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import classNames from "classnames";
+import { Collapse, NavItem, NavLink } from "reactstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
+
+const SubMenu = (props) => {
+  const [collapsed, setCollapsed] = useState(true);
+  const toggle = () => setCollapsed(!collapsed);
+  const { icon, title, items } = props;
+
+  return (
+    <div>
+      <NavItem
+        onClick={toggle}
+        className={classNames({ "menu-open": !collapsed })}
+      >
+        <NavLink className="dropdown-toggle">
+          <FontAwesomeIcon icon={icon} className="mr-2" />
+          {title}
+        </NavLink>
+      </NavItem>
+      <Collapse
+        isOpen={!collapsed}
+        navbar
+        className={classNames("items-menu", { "mb-1": !collapsed })}
+      >
+        {items.map((item, index) => (
+          <NavItem key={index} className="pl-4">
+            <NavLink tag={Link} to={item.target}>
+              {item.title}
+            </NavLink>
+          </NavItem>
+        ))}
+      </Collapse>
+    </div>
+  );
+};
+
+export default SubMenu;
