43cfdaffcdee3d35c114398c7d70cb17f47dbb8f
diyorbek0309 <diyorbekolimov2000@gmail.com>
2022-02-21 10:01:44 +0500

    removed unused items

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import { useState } from "react";
-import {
-  Routes,
-  Route
-} from "react-router-dom";
-import Login from './components/login/Login';
+import { Routes, Route } from "react-router-dom";
+import Login from "./components/login/Login";
 import SideBar from "./components/sidebar/SideBar";
-import Topbar from "./components/sidebar/TopBar";
-import { PrivateRoute } from './utils/PrivateRoute';
-import Content from './components/content/Content';
-import './App.css'
+import Content from "./components/content/Content";
+import "./App.css";
 
 import "bootstrap/dist/css/bootstrap.min.css";
-import Home from "./components/home/Home";
 
 function App() {
   const [sidebarIsOpen, setSidebarOpen] = useState(true);
@@ -21,9 +15,7 @@ function App() {
       <SideBar toggle={toggleSidebar} isOpen={sidebarIsOpen} />
       <Content toggleSidebar={toggleSidebar} sidebarIsOpen={sidebarIsOpen} />
       <Routes>
-        {/* <Route path="/" element={<PrivateRoute>yrty</PrivateRoute>}/> */}
-        <Route path="/login" element={<Login />}/>
-        {/* <Route path="/home" element={<PrivateRoute><Home /></PrivateRoute>}/> */}
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   );
--- src/components/sidebar/TopBar.jsx
+++ src/components/sidebar/TopBar.jsx
@@ -1,16 +1,7 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAlignLeft } from "@fortawesome/free-solid-svg-icons";
-import {
-  Navbar,
-  Button,
-  NavbarToggler,
-  Collapse,
-  Nav,
-  NavItem,
-  NavLink,
-} from "reactstrap";
-import { Link } from "react-router-dom";
+import { Navbar, Button, NavbarToggler } from "reactstrap";
 
 const Topbar = ({ toggleSidebar }) => {
   const [topbarIsOpen, setTopbarOpen] = useState(true);
@@ -27,30 +18,6 @@ const Topbar = ({ toggleSidebar }) => {
         <FontAwesomeIcon icon={faAlignLeft} />
       </Button>
       <NavbarToggler onClick={toggleTopbar} />
-      <Collapse isOpen={topbarIsOpen} navbar>
-        <Nav className="ml-auto" navbar>
-          <NavItem>
-            <NavLink tag={Link} to={"/page-1"}>
-              page 1
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink tag={Link} to={"/page-2"}>
-              page 2
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink tag={Link} to={"/page-3"}>
-              page 3
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink tag={Link} to={"/page-4"}>
-              page 4
-            </NavLink>
-          </NavItem>
-        </Nav>
-      </Collapse>
     </Navbar>
   );
 };
